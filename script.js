e01786324150813bb384d15bd7b5459f3523680a
AbdulBasit4441 <basid1496@gmail.com>
2025-04-27 09:58:31 +0500

    adding my small portfolio object

---
+++ script.js
@@ -0,0 +1,87 @@
+var tl = gsap.timeline() 
+
+tl.from(".nav_l h2",{
+    y:-30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.3
+})
+tl.from(".nav_l a",{
+    y:-30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.3
+})
+tl.from(".nav-r ",{
+    y:-30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.3
+})
+tl.from(".dash-l h1",{
+    x:-30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.3
+})
+
+tl.from(".dash-r",{
+    x:30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.3
+})
+tl.from(".dash-r h3",{
+    x:30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.3
+})
+tl.from(".dash-r h2",{
+    x:30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.3
+})
+tl.from(".dash-r p",{
+    x:30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.3
+})
+tl.from(".dash-r a",{
+    x:30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.3
+})
+tl.from(".onemore i",{
+    x:-60,
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+})
+tl.from(".bot_lef i",{
+    y:30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+})
+tl.from(".bot_lef h3",{
+    y:30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+})
+tl.from(".bot_lef a",{
+    y:30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+})
+tl.from(".bot_rig h3",{
+    y:30,
+    opacity:0,
+    duration:0.5,
+    stagger:0.2
+})
